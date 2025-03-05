e89ad4f7918033e411682add1b1d00fa5d7dc9a9
CodingPotatoes <williamernestarcher@gmail.com>
2025-03-04 20:11:54 -0500

    Added toStrings to AutoCommands, still a wip

--- src/competition/opcontrol.cpp
+++ src/competition/opcontrol.cpp
@@ -4,17 +4,36 @@
 #include "vex.h"
 
 void testing();
-
+void konamiCode();
+void inputCheck();
 void auto__();
 
 /**
  * Main entrypoint for the driver control period
  */
+
+enum INPUTS{
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+    B,
+    A,
+    X,
+    Y,
+    R1,
+    R2,
+    L1,
+    L2,
+};
+std::vector<INPUTS> inputTracker;
+bool runDrive = true;
+
 void opcontrol() {
     // vexDelay(1000);
     // autonomous();
     // return;
-    // testing();
+    testing();
     wallstakemech_sys.hold = false;
     // intake_sys.conveyor_stop();
     intake_sys.setLight(false);
@@ -36,6 +55,7 @@ void opcontrol() {
     conveyor_button.released([]() {
         intake_sys.intake_stop();
         intake_sys.conveyor_stop();
+        
     });
 
     conveyor_button_rev.released([]() {
@@ -56,27 +76,152 @@ void opcontrol() {
         wallstakemech_sys.hold = true;
         wallstakemech_sys.set_setpoint(from_degrees(200));
     });
+    //stupid shit
+    inputCheck();
 
     // ================ INIT ================
-
     while (true) {
         if (!conveyor_button.pressing() && !conveyor_button_rev.pressing()) {
             intake_sys.intake_stop();
             intake_sys.conveyor_stop();
         }
-        OdometryBase *odombase = &odom;
-        pose_t pos = odombase->get_position();
-        printf("ODO X: %.2f, Y: %.2f, R:%.2f, Concurr: %f\n", pos.x, pos.y, pos.rot, conveyor.current());
+        konamiCode();
+
+        // OdometryBase *odombase = &odom;
+        // pose_t pos = odombase->get_position();
+        // printf("ODO X: %.2f, Y: %.2f, R:%.2f, Concurr: %f\n", pos.x, pos.y, pos.rot, conveyor.current());
 
         double left = (double)con.Axis3.position() / 100;
         double right = (double)con.Axis2.position() / 100;
-
-        drive_sys.drive_tank(left, right, 1, TankDrive::BrakeType::None);
+        if(runDrive){
+            drive_sys.drive_tank(left, right, 1, TankDrive::BrakeType::None);
+        }
 
         vexDelay(20);
     }
 
     // ================ PERIODIC ================
+}
+
+void printInput(){
+    for(int i = 0; i <inputTracker.size(); i++){
+        switch(inputTracker.at(i)){
+            case UP:
+                printf("UP, ");
+                break;
+            case DOWN:
+                printf("DOWN, ");
+                break;
+            case LEFT:
+                printf("LEFT, ");
+                break;
+            case RIGHT:
+                printf("RIGHT, ");
+                break;
+            case A:
+                printf("A, ");
+                break;
+            case B:
+                printf("B, ");
+                break;
+            case X:
+                printf("X, ");
+                break;
+            case Y:
+                printf("Y, ");
+                break;
+            case R1:
+                printf("R1, ");
+                break;
+            case R2:
+                printf("R2, ");
+                break;
+            case L1:
+                printf("L1, ");
+                break;
+            case L2:
+                printf("L2, ");
+                break;
+                default:
+                printf("unkown, ");
+                break;
+
+        }
+    }
+    printf("\n");
+}
+
+void inputCheck(){
+    con.ButtonX.pressed([](){
+        inputTracker.push_back(X);
+        printInput();
+    });
+    con.ButtonY.pressed([](){
+        inputTracker.push_back(Y);
+        printInput();
+    });
+    con.ButtonA.pressed([](){
+        inputTracker.push_back(A);
+        printInput();
+    });
+    con.ButtonB.pressed([](){
+        inputTracker.push_back(B);
+        printInput();
+    });
+    con.ButtonLeft.pressed([](){
+        inputTracker.push_back(LEFT);
+        printInput();
+    });
+    con.ButtonRight.pressed([](){
+        inputTracker.push_back(RIGHT);
+        printInput();
+    });
+    con.ButtonUp.pressed([](){
+        inputTracker.push_back(UP);
+        printInput();
+    });
+    con.ButtonDown.pressed([](){
+        inputTracker.push_back(DOWN);
+        printInput();
+    });
+    con.ButtonL1.pressed([](){
+        inputTracker.push_back(L1);
+        printInput();
+    });
+    con.ButtonL2.pressed([](){
+        inputTracker.push_back(L2);
+        printInput();
+    });
+    con.ButtonR1.pressed([](){
+        inputTracker.push_back(R1);
+        printInput();
+    });
+    con.ButtonR2.pressed([](){
+        inputTracker.push_back(R2);
+        printInput();
+    });
+}
+
+void konamiCode(){
+    if(inputTracker.size() >= 9){
+        if((inputTracker.at(0) == UP) && (inputTracker.at(1) == DOWN)  && (inputTracker.at(2) == UP) && (inputTracker.at(3) == DOWN)
+        && (inputTracker.at(4) == LEFT) && (inputTracker.at(5) == RIGHT) && (inputTracker.at(6) == B) && (inputTracker.at(7) == A)
+        && (inputTracker.at(8) == R2)){
+            if(runDrive){
+                printf("Turning off drive\n");
+                runDrive = false;
+            }
+            else if(!runDrive){
+                printf("Turning on drive\n");
+                runDrive = true;
+            }
+            inputTracker.clear();
+        }
+    }
+    if(inputTracker.size() > 9){
+        inputTracker.erase(inputTracker.begin());
+    }
+
 }
 
 void testing() {
@@ -101,24 +246,27 @@ void testing() {
     };
 
     con.ButtonX.pressed([]() {
-        printf("running test");
-        CommandController cc{
-          new Async(new FunctionCommand([]() {
-              while (true) {
-                  printf(
-                    "ODO X: %f ODO Y: %f, ODO ROT: %f TURNPID ERROR: %f\n", odom.get_position().x,
-                    odom.get_position().y, odom.get_position().rot, turn_pid.get_error()
-                  );
-                  vexDelay(100);
-              }
-              return true;
-          })),
-          drive_sys.TurnDegreesCmd(15, 1),
-          // drive_sys.TurnDegreesCmd(30, 1)->withTimeout(3),
-          // drive_sys.TurnDegreesCmd(45, 1)->withTimeout(3),
-          // drive_sys.TurnDegreesCmd(90, 1)->withTimeout(3),
-          // drive_sys.TurnDegreesCmd(180, 1)->withTimeout(3),
-        };
-        cc.run();
+        if(!runDrive){
+            printf("running test");
+            CommandController cc{
+            new Async(new FunctionCommand([]() {
+                while (true) {
+                    printf(
+                        "ODO X: %f ODO Y: %f, ODO ROT: %f TURNPID ERROR: %f\n", odom.get_position().x,
+                        odom.get_position().y, odom.get_position().rot, turn_pid.get_error()
+                    );
+                    vexDelay(100);
+                }
+                return true;
+            })),
+            drive_sys.TurnDegreesCmd(90, 1),
+            // drive_sys.TurnDegreesCmd(30, 1)->withTimeout(3),
+            // drive_sys.TurnDegreesCmd(45, 1)->withTimeout(3),
+            // drive_sys.TurnDegreesCmd(90, 1)->withTimeout(3),
+            // drive_sys.TurnDegreesCmd(180, 1)->withTimeout(3),
+            };
+            cc.run();
+        }
     });
+        
 }
