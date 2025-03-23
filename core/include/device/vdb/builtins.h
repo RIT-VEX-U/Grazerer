08fa9c5c7fcb22fdf9e21565e3824eb05d03daac
CodingPotatoes <williamernestarcher@gmail.com>
2025-03-23 14:54:14 -0400

    setting up for debug board

---
+++ core/include/device/vdb/builtins.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../core/include/device/vdb/types.h"
+#include <memory>
+
+#include "vex.h"
+namespace VDP {
+
+class Timestamped : public Record {
+  public:
+    Timestamped(std::string name, Part *data);
+
+    void fetch();
+
+  private:
+    std::shared_ptr<Uint32> timestamp;
+    PartPtr data;
+};
+
+class Motor : public Record {
+  public:
+    Motor(std::string name, vex::motor &mot);
+    void fetch() override;
+
+  private:
+    vex::motor &mot;
+
+    std::shared_ptr<Float> pos;
+    std::shared_ptr<Float> vel;
+    std::shared_ptr<Uint8> temp;
+    std::shared_ptr<Float> voltage;
+    std::shared_ptr<Float> current;
+};
+} // namespace VDP
