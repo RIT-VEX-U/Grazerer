36248a32306b609bf5c70661675cbf1c9fecd7f9
NikNikskywalker <nikolaihixon@gmail.com>
2025-04-03 21:43:39 -0400

    Possibly Added Penalties
    Untested, just took last code and updated classes to use modern implementation.

--- include/mazegame/mazegame.h
+++ include/mazegame/mazegame.h
@@ -8,8 +8,8 @@ namespace MazeGame
 
 typedef struct line_s
 {
-  line_s(Vector::point_t pt1, Vector::point_t pt2) : point1(pt1), point2(pt2) {}
-  Vector::point_t point1, point2;
+  line_s(Translation2d pt1, Translation2d pt2) : point1(pt1), point2(pt2) {}
+  Translation2d point1, point2;
 } line_t;
 
 extern std::vector<line_t> penalty_list, smup_list;
--- src/mazegame/mazegame.cpp
+++ src/mazegame/mazegame.cpp
@@ -9,30 +9,30 @@ int MazeGame::num_penalties = 0;
 void MazeGame::init_boundary_lines()
 {
   // DQ lines
-  smup_list.push_back({{.x=23.5, .y=0}, {.x=23.5,.y=47}});
-  smup_list.push_back({{.x=23.5, .y=47}, {.x= 47, .y=47}});
-  smup_list.push_back({{.x=47, .y=47}, {.x=47, .y=118}});
+  smup_list.push_back({{23.5, 0}, {23.5, 47}});
+  smup_list.push_back({{23.5, 47}, {47, 47}});
+  smup_list.push_back({{47, 47}, {47, 118}});
 
   // Penalty Lines
-  penalty_list.push_back({{.x=0, .y=68}, {.x=21, .y=68}});
-  penalty_list.push_back({{.x=23, .y=92}, {.x=44, .y=92}});
-  penalty_list.push_back({{.x=23, .y=92}, {.x=23, .y=114}});
-  penalty_list.push_back({{.x=23, .y=114}, {.x=70, .y=114}});
-  penalty_list.push_back({{.x=50, .y=22}, {.x=75, .y=22}});
-  penalty_list.push_back({{.x=75, .y=23}, {.x=75, .y=40}});
+  penalty_list.push_back({{0, 68}, {21, 68}});
+  penalty_list.push_back({{23, 92}, {44, 92}});
+  penalty_list.push_back({{23, 92}, {23, 114}});
+  penalty_list.push_back({{23, 114}, {70, 114}});
+  penalty_list.push_back({{50, 22}, {75, 22}});
+  penalty_list.push_back({{75, 23}, {75, 40}});
 }
 
 bool MazeGame::is_single_penalty()
 {
   static bool was_prev_penalty = false;
 
-  position_t pos = odom.get_position();
-  std::vector<Vector::point_t> intersect;
+  Pose2d pos = odom.get_position();
+  std::vector<Translation2d> intersect;
 
   bool is_penalty = false;
   for(line_t l : penalty_list)
   {
-    intersect = PurePursuit::line_circle_intersections({.x=pos.x, .y=pos.y}, ROBOT_RADIUS, l.point1, l.point2);
+    intersect = PurePursuit::line_circle_intersections({pos.x(), pos.y()}, ROBOT_RADIUS, l.point1, l.point2);
 
     if(intersect.size() > 0)
     {
@@ -43,7 +43,7 @@ bool MazeGame::is_single_penalty()
 
   if(!was_prev_penalty && is_penalty)
   {
-    main_controller.rumble(".");
+    // main_controller.rumble(".");
     num_penalties++;
   }
   
@@ -56,13 +56,13 @@ bool MazeGame::is_super_mega_ultra_penalty()
 {
   static bool was_prev_penalty = false;
 
-  position_t pos = odom.get_position();
-  std::vector<Vector::point_t> intersect;
+  Pose2d pos = odom.get_position();
+  std::vector<Translation2d> intersect;
 
   bool is_penalty = false;
   for(line_t l : smup_list)
   {
-    intersect = PurePursuit::line_circle_intersections({.x=pos.x, .y=pos.y}, ROBOT_RADIUS, l.point1, l.point2);
+    intersect = PurePursuit::line_circle_intersections({pos.x(), pos.y()}, ROBOT_RADIUS, l.point1, l.point2);
 
     if(intersect.size() > 0)
     {
